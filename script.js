2b6bfa5afe047bb2a3fd5c11efeebd90cb67a3f4
Umberto Zappulla <136740583+Umberto2698@users.noreply.github.com>
2023-07-28 17:11:53 +0200

    Added style

---
+++ script.js
@@ -0,0 +1,10 @@
+const header = document.getElementsByClassName("header")[0];
+const button = document.getElementById("scroll-anchor");
+
+header.addEventListener("scroll", (event) => {
+  console.log(event);
+});
+
+onscroll = (event) => {
+  console.log(event);
+};
